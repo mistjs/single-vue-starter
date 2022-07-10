eb33d54fdd556f92afbde69390c4d9ef4b150c72
aibayanyu <aibayanyu@qq.com>
2022-07-11 07:37:23 +0800

    feat: add single vue starter

---
+++ scripts/build.ts
@@ -0,0 +1,9 @@
+const main = async() => {
+  // TODO
+}
+
+main().then(() => {
+  // success
+}).catch(() => {
+  // ERROR
+})
---
+++ site/vite.config.ts
@@ -0,0 +1,10 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+})
